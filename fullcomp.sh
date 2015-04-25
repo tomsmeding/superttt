095becbc1c41472ae23238f087aaa9b556df2124
tomsmeding <hallo@tomsmeding.nl>
2015-04-25 21:12:31 +0200

    Integrate stttswag into makefile flow

--- fullcomp.sh
+++ fullcomp.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-for p1 in ./stttfirst ./stttrandom ./stttrecur; do
-	for p2 in ./stttfirst ./stttrandom ./stttrecur; do
+for p1 in ./stttfirst ./stttrandom ./stttrecur ./stttswag; do
+	for p2 in ./stttfirst ./stttrandom ./stttrecur ./stttswag; do
 		if [[ $p1 == $p2 ]]; then
 			continue
 		fi
