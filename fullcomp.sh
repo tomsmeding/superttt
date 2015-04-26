343344ba27c758d065f43fe52378e9e039ee6ebf
tomsmeding <hallo@tomsmeding.nl>
2015-04-26 07:42:33 +0200

    Improve competition handling and stats

--- fullcomp.sh
+++ fullcomp.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+find competitions -type f -delete
+
 for p1 in ./stttfirst ./stttrandom ./stttrecur ./stttswag; do
 	for p2 in ./stttfirst ./stttrandom ./stttrecur ./stttswag; do
 		if [[ $p1 == $p2 ]]; then
@@ -14,44 +16,4 @@ for p1 in ./stttfirst ./stttrandom ./stttrecur ./stttswag; do
 	done
 done
 
-#CANNOT USE TABS BELOW -- SORRY!
-python3 <<EOF
-from os import listdir
-from os.path import isfile,join
-from re import match
-from sys import exit
-files=[fnm for fnm in listdir("competitions") if isfile(join("competitions",fnm)) and fnm[:5]=="game_"]
-scores=dict()
-playernames=[]
-for file in files:
-   f=open(join("competitions",file))
-   lines=f.readlines()
-   p1name=lines[0][4:].strip()
-   p2name=lines[1][4:].strip()
-   result=lines[-1].strip()
-   if not match(r"P. won|Tie",result):
-      print("Unterminated log file "+file)
-      exit(1)
-   try:
-      playernames.index(p1name)
-   except:
-      playernames.append(p1name)
-      scores[p1name]=0
-   try:
-      playernames.index(p2name)
-   except:
-      playernames.append(p2name)
-      scores[p2name]=0
-   if result=="Tie":
-      scores[p1name]+=1
-      scores[p2name]+=1
-   elif result=="P1 won":
-      scores[p1name]+=3
-      scores[p2name]+=1
-   elif result=="P1 won":
-      scores[p1name]+=1
-      scores[p2name]+=3
-scores=sorted(scores.items(),key=lambda item:-item[1]) #sort reverse on scores
-for score in scores:
-   print(score[0]+": "+str(score[1]))
-EOF
+./fullcompstats.py
